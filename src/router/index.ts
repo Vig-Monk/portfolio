8f17815eae8c7d841ecdefc053fdc78d5c6f5341
Ludwig <ludwigmaingi@gmail.com>
2026-06-21 20:22:14 +0300

    Fix: Scroll behaviour on router/index.js

--- src/router/index.ts
+++ src/router/index.ts
@@ -18,9 +18,10 @@ const router = createRouter({
   history: createWebHistory(),
   routes,
   scrollBehavior(to, _from, savedPosition) {
-    if (savedPosition) return savedPosition;
-    return { top: 0 };
-  }
+  if (savedPosition) return savedPosition;
+  if (to.hash) return { el: to.hash, behavior: "smooth" };
+  return { top: 0 };
+}
 });
 
 export default router;
